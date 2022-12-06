6f7a9efc47b01875ee3e8cb945657556a454ebea
GDel84 <g-del@hotmail.fr>
2022-12-06 10:38:16 +0100

    symfony initialization

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+
+{
+    public function index()
+    {
+        return new Response('Hello world!');
+    }
+}
