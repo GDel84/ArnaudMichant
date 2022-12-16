ebd2e989b96e1e480a8d477f7debd5889a6f829b
GDel84 <g-del@hotmail.fr>
2022-12-16 14:48:22 +0100

    nav admin + public

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 
 {
-    public function index()
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
     {
-        return new Response('Hello world!');
+        return $this->render('accueil.html.twig', [
+            'accueil_name' => 'PublicController',
+        ]);
     }
 }
