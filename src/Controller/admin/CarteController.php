d2a83184583e31ff98e2a74b9905e7d7d958e249
GDel84 <g-del@hotmail.fr>
2022-12-25 15:51:18 +0100

    Carte admin

---
+++ src/Controller/admin/CarteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarteController extends AbstractController
+{
+    #[Route('/carte', name: 'app_carte')]
+    public function index(): Response
+    {
+        return $this->render('carte/admin-carte.html.twig', [
+            'controller_name' => 'CarteController',
+        ]);
+    }
+}
