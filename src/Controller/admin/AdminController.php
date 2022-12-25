16641878230a6e4df8e0c666d4a40b29d4c43ca3
GDel84 <g-del@hotmail.fr>
2022-12-25 15:52:15 +0100

    schedule admin

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\admin;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
