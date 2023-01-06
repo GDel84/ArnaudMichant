de707ad63adfbdf38ff70fae37cda35d550a67b0
GDel84 <g-del@hotmail.fr>
2023-01-06 16:35:29 +0100

    method pucture create edit

--- src/Controller/admin/PictureController.php
+++ src/Controller/admin/PictureController.php
@@ -2,17 +2,107 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Photo;
+use App\Form\PictureFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class PictureController extends AbstractController
 {
-    #[Route('/admin/picture', name: 'app_picture')]
-    public function index(): Response
+    #[Route('/admin/picture/create', name: 'admin-picture')]
+    public function picture(SluggerInterface $slugger, Request $request): Response
     {
-        return $this->render('picture/admin-picture.html.twig', [
-            'controller_name' => 'PictureController',
+        $pic = new Photo();
+        $form = $this->createForm(PictureFormType::class, $pic);
+        $form->handleRequest($request);
+
+        $picfile = $form->get('picture')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($picfile) {
+                $originalFilename = pathinfo($picfile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$picfile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $picfile->move(
+                        $this->getParameter('picture_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $pic->setPicture($newFilename);
+
+                $pic->setPicture(
+                    new File($this->getParameter('picture_directory').'/'.$pic->getPicture())
+                );
+            }
+
+            // ... persist the $product variable or any other work
+
+        return $this->render('admin/picture/admin-picture-create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/picture/edit/{id}', name: 'admin-picture-modifier')]
+    public function PictureEdit(ManagerRegistry $doctrine, $id, SluggerInterface $slugger, Request $request)
+    {
+
+        $picRepo = $doctrine->getRepository(Photo::class);
+        $pic = $picRepo->findOneBy(['id'=>$id]);
+        $form = $this->createForm(PictureFormType::class, $pic);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $picFile = $form->get('picture')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($picFile) {
+                $originalFilename = pathinfo($picFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$picFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $picFile->move(
+                        $this->getParameter('picture_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $pic->setImage($newFilename);
+
+                $em = $doctrine->getManager();
+                $em->persist($pic);
+                $em->flush();
+            }
+            return $this->redirectToRoute('admin');
+        }
+        
+        return $this->render('admin/picture/admin-picture-edit.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/PictureFormType.php
+++ src/Form/PictureFormType.php
@@ -4,8 +4,11 @@ namespace App\Form;
 
 use App\Entity\Photo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class PictureFormType extends AbstractType
 {
@@ -13,8 +16,34 @@ class PictureFormType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('picture')
+            ->add('picture', FileType::class, [
+                'label' => 'image (jpg file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Vérifiez que vos image soi au format jpg ou jpeg',
+                    ])
+                ],
+            ])
             ->add('pictureOrder')
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'modif'],
+                "label" => 'enregister'
+                ])
         ;
     }
 
