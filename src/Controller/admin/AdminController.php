b91f500c88c8a9b42c9ad39deb00edbd00bb2ec0
GDel84 <g-del@hotmail.fr>
2023-01-08 13:45:21 +0100

    add rendered

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(PhotoRepository $picRepo): Response
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'pictures' => $picRepo->findAll(),
+            
         ]);
     }
 }
