8097455a0a2b5f67715933d9dc2d450abba3b8fa
GDel84 <g-del@hotmail.fr>
2023-01-15 17:01:28 +0100

    add tostring

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -183,4 +183,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString()
+    {
+        return $this->getName();
+    }
 }
