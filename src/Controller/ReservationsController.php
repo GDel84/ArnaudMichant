a75254bdba0c912c1e3116d6bfdebbe7e5efb338
GDel84 <g-del@hotmail.fr>
2023-01-15 17:04:32 +0100

    create the methode

---
+++ src/Controller/ReservationsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reservations;
+use App\Form\ReservationsFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationsController extends AbstractController
+{
+    #[Route('/reservation', name: 'reservation')]
+    public function reservations(ManagerRegistry $doctrine, Request $request)
+    {
+        $reservation = new Reservations();
+        $form = $this->createForm(ReservationsFormType::class, $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $doctrine->getManager();
+            $em->persist($reservation);
+            $em->flush(); 
+
+            return $this->redirectToRoute('accueil');
+
+        }
+        return $this->render('reservation.html.twig', [
+            'reservationForm' => $form->createView(),
+        ]);
+    }
+}
