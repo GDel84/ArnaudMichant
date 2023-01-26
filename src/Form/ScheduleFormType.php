007e655d71dcb404c3ee9b3db6788af6aaced3a6
GDel84 <g-del@hotmail.fr>
2023-01-26 17:37:43 +0100

    add week for number

--- src/Form/ScheduleFormType.php
+++ src/Form/ScheduleFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Schedule;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,22 @@ class ScheduleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('week')
-            ->add('scheduleMoon')
-            ->add('scheduleNight')
+            ->add('week', ChoiceType::class, [
+                'choices'  => [
+                    'Lundi' => 1,
+                    'Mardi' => 2,
+                    'Mercredi' => 3,
+                    'Jeudi' => 4,
+                    'Vendredi' => 5,
+                    'Samedi' => 6,
+                    'Dimanche' => 0,
+                ],
+            ])
+            ->add('noonStartTime')
+            ->add('noonEndTime')
+            ->add('nightStartTime')
+            ->add('nightEndTime')
+
         ;
     }
 
