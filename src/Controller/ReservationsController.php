6b8c859de83d429774f66004f8b91e36a370fe20
GDel84 <g-del@hotmail.fr>
2023-02-04 15:51:45 +0100

    flesh form Reservation by form main

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -3,24 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Reservations;
+use App\Entity\User;
 use App\Form\ReservationsFormType;
 use App\Repository\ScheduleRepository;
+use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ReservationsController extends AbstractController
 {
-    #[Route('/reservation', name: 'reservation')]
-    public function reservations(ManagerRegistry $doctrine, Request $request, ScheduleRepository $scheduleRepo)
+    #[Route('/reservation', name: 'reservation', methods:['POST', 'GET'])] 
+    public function reservations(ManagerRegistry $doctrine, Request $request, ScheduleRepository $scheduleRepo, Security $security)
     {
-        $reservation = new Reservations();
-        $form = $this->createForm(ReservationsFormType::class, $reservation);
-        $form->handleRequest($request);
+        $user = $security->getUser();
+        
+        if ($request->get('user_scheduled')) {
+            $reservation = new Reservations;
+            $dateReservation = DateTime::createFromFormat('Y-m-d H:i', $request->get('user_scheduled').' '.$request->get('time'));
 
-        if ($form->isSubmitted() && $form->isValid()) {
+            $reservation->setName($request->get('user_name'));
+            $reservation->setLastName($request->get('user_LastName'));
+            $reservation->setEmail($request->get('user_email'));
+            $reservation->setNbcouverts($request->get('nbcouverts'));
+            $reservation->setMentionsAllergene($request->get('mentions_allergene'));
+            $reservation->setScheduledTime($dateReservation);
+            if ($user){
+                $reservation->setUserResa($user);
+            }
 
             $em = $doctrine->getManager();
             $em->persist($reservation);
@@ -30,8 +43,8 @@ class ReservationsController extends AbstractController
 
         }
         return $this->render('reservation.html.twig', [
-            'reservationForm' => $form->createView(),
             'schedules' => $scheduleRepo->findAll(),
+            'user'=> $user,
         ]);
     }
 }
