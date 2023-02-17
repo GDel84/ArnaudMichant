ba28fbb56280c21f337a21e3a2081375f68ca435
GDel84 <g-del@hotmail.fr>
2023-02-17 10:10:06 +0100

    type user for admin

---
+++ src/Form/UserAdminFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserAdminFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('Name')
+            ->add('firstName')
+            ->add('mentionsAllergene')
+            ->add('nbCouverts')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
